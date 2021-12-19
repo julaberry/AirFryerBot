950c6448f71d1db8da93a2c25a21f5f6194b9a32
Jenn <chrystamofdem@gmail.com>
2021-12-19 13:41:16 -0800

    Added image functionality using Pillow

--- bot.py
+++ bot.py
@@ -5,14 +5,28 @@ from discord.ext import commands
 from PIL import Image
 
 load_dotenv("config.env")
-TOKEN = getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")
 
 bot = commands.Bot(command_prefix='$')
 
+bottom = Image.open('bottomtext.png')
 
-
-#im1 = Image.open('data/src/lena.jpg')
-im2 = Image.open('data/src/rocket.jpg')
+#https://note.nkmk.me/en/python-pillow-concat-images/
+def get_concat_v_resize(im1, im2, resample=Image.BICUBIC, resize_big_image=False):
+	if im1.width == im2.width:
+		_im1 = im1
+		_im2 = im2
+	elif (((im1.width > im2.width) and resize_big_image) or
+		  ((im1.width < im2.width) and not resize_big_image)):
+		_im1 = im1.resize((im2.width, int(im1.height * im2.width / im1.width)), resample=resample)
+		_im2 = im2
+	else:
+		_im1 = im1
+		_im2 = im2.resize((im1.width, int(im2.height * im1.width / im2.width)), resample=resample)
+	dst = Image.new('RGB', (_im1.width, _im1.height + _im2.height))
+	dst.paste(_im1, (0, 0))
+	dst.paste(_im2, (0, _im1.height))
+	return dst
 
 
 @bot.event
@@ -22,12 +36,32 @@ async def on_ready():
 
 @bot.command()
 async def clearly(ctx, imagenum = 0):
-    message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-    try:
-    	message.attachments[0].url
-    	await ctx.send(message.attachments[0])
-    except:
-    	await ctx.send("Attachment " + str(imagenum) + " does not exist")
+	try:
+		message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+	except:
+		await ctx.send("Reply to an image with this command.", delete_after=10)
+		return
+	try:
+		attachment = message.attachments[0]
+		await attachment.save("tempimage")
+		#await ctx.send(message.attachments[0])
+	except Exception as e:
+		print(e)
+		await ctx.send("Attachment " + str(imagenum) + " does not exist", delete_after=10)
+		return
+	try:
+		top = Image.open("tempimage")
+		get_concat_v_resize(top, bottom).save("meme.jpg")
+		await ctx.send(file=discord.File('meme.jpg'))
+	except Exception as e:
+		print(e)
+		await ctx.send("Error in processing.")
+		return
+	finally:
+		if os.path.exists("tempimage"):
+			os.remove("tempimage")
+		if os.path.exists("meme.jpg"):
+			os.remove("meme.jpg")
    
 
 
